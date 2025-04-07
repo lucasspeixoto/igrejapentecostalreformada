d938dd0ac6de8519269a21e96bb4e936c979eff2
Lucas Peixoto <lspeixotodev@gmail.com>
2025-04-06 21:40:32 -0300

    Fix weird margins in auth views and remove layout background image

--- src/app/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/auth/components/forgot-password/forgot-password.component.ts
@@ -35,8 +35,9 @@ import {
     <p-toast />
     <div
       class="container bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
-      <div class="flex flex-col items-center justify-center">
+      <div class="w-full flex flex-col items-center justify-center">
         <div
+          class="w-[90%] sm:w-[400px]"
           style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
           <div
             class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20"
@@ -60,7 +61,7 @@ import {
                   id="emailField"
                   type="text"
                   placeholder="Endereço de Email"
-                  class="w-full md:w-[30rem] mb-2"
+                  class="w-full mb-2"
                   formControlName="email" />
 
                 <app-custom-validation-message id="emailErrorMessage" controlName="email" />
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -30,11 +30,12 @@ import { CustomValidationMessageComponent } from '../../../components/custom-val
   template: `
     <div
       class="container bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
-      <div class="flex flex-col items-center justify-center">
+      <div class="w-full flex flex-col items-center justify-center">
         <div
+          class="w-[90%] sm:w-[400px]"
           style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
           <div
-            class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20"
+            class="bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20"
             style="border-radius: 53px">
             <div class="text-center mb-8">
               <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">
@@ -55,7 +56,7 @@ import { CustomValidationMessageComponent } from '../../../components/custom-val
                   id="emailField"
                   type="text"
                   placeholder="Endereço de Email"
-                  class="w-full md:w-[30rem] mb-2"
+                  class="w-full mb-2"
                   formControlName="email" />
                 <app-custom-validation-message id="emailErrorMessage" controlName="email" />
               </div>
